47e9feb3492b28cc2dd8f9a4ba5b6c954b1ccb1c
Mahmoud Nagah <mahmoud.nagah87@gmail.com>
2018-09-22 17:00:36 +0200

    Tem_task

---
+++ blancher/Services/TEMP_Task.c
@@ -0,0 +1,26 @@
+/*
+ * TEMP_Task.c
+ *
+ * Created: 9/22/2018 4:24:23 PM
+ *  Author: M.nagah
+ */ 
+#include "TEMP_Task.h"
+#include "../ECUAL/Temperature.h"
+#include "../ECUAL/LCD.h"
+static uint16_t s_current_temp;
+/*
+*Set the Temperatur on RTE
+*Parameters @ Nothing
+*return Nothing 
+*/
+void Temp_main(void* pvParameters){
+	temp_init(0);
+	while (1)
+	{
+		s_current_temp = temp_read();
+		//set on RTE
+		//vTaskDelay()
+		_delay_ms(1000);
+	}
+
+}
