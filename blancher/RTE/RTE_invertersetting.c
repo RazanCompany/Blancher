131ed066d936db6cb82dc024f16de69a40fef126
Mohab Adel <mohabadel1905@gmail.com>
2018-09-22 07:09:09 -0700

    lcd task service layer

--- blancher/RTE/RTE_invertersetting.c
+++ blancher/RTE/RTE_invertersetting.c
@@ -4,3 +4,9 @@
  * Created: 9/20/2018 3:14:40 AM
  *  Author: Mohab Adel
  */ 
+// gear ratio r
+// rpm max  r
+// distance  r
+// diameter  r
+// time minute r
+// time second  r
--- blancher/RTE/RTE_temperature.c
+++ blancher/RTE/RTE_temperature.c
@@ -4,3 +4,9 @@
  * Created: 9/20/2018 3:09:34 AM
  *  Author: Mohab Adel
  */ 
+// sleep temp r
+// set temp r
+// three shold r
+// offest+ r
+// offset- r
+// current temp w
