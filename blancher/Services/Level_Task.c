e3bdaf3b542f4da63f050c23e716eec1d3623d59
mahmoud <elgendy>
2018-09-26 14:51:58 +0200

    add task_level

---
+++ blancher/Services/Level_Task.c
@@ -0,0 +1,47 @@
+﻿/*
+ * Level_Task.c
+ *
+ * Created: 26/09/2018 01:00:54 م
+ *  Author: Mahmoud Elgendy 
+ */ 
+
+
+#include "Level_Task.h"
+#include "../RTOS_Includes.h"
+#include "tank_operation.h"
+#include "../ECUAL/levels.h"
+#include "../RTE/RTE_tankslevels.h"
+#include "../RTE/RTE_error_types.h"
+
+static uint8_t g_tank_level = 0;
+static uint8_t g_blancher_level = 0;
+
+void Level_task (void* pvParameters )
+{
+	while (1)
+	{
+		g_tank_level = Get_tank_level();
+		g_blancher_level = Get_blancher_level();
+		if (Check_for_sensor_error())
+		{
+			RTE_set_tank_levels(INVALID_DATA);
+		}
+		RTE_set_tank_level(g_tank_level);
+		RTE_set_blancher_level(g_blancher_level);
+		
+		vTaskDelay(200/portTICK_PERIOD_MS) ;
+	}
+}
+
+static uint8_t Check_for_sensor_error(void)
+{
+   if (g_tank_level != 0 || g_tank_level != 1 || g_tank_level != 3 || g_tank_level != 7  )	
+   {
+	   return 1 ;
+   }
+   return 0 ;
+}
+
+
+
+
---
+++ blancher/Services/Level_Task.h
@@ -0,0 +1,16 @@
+﻿/*
+ * Level_Task.h
+ *
+ * Created: 26/09/2018 01:00:35 م
+ *  Author: ENG
+ */ 
+
+
+#ifndef LEVEL_TASK_H_
+#define LEVEL_TASK_H_
+
+void Level_task (void* pvParameters );
+static uint8_t Check_for_sensor_error(void);
+
+
+#endif /* LEVEL_TASK_H_ */
